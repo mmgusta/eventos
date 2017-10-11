1bd1541308806b304e97a82eb565c2479a81d52f
gustavorpacheco <32715242+gustavorpacheco@users.noreply.github.com>
2017-10-11 16:21:51 -0300

    Rotas e NavBar
    Rotas amigaveis e NavBar com as Views sem Bootstrap

--- FastTickets/FastTickets/App_Start/RouteConfig.cs
+++ FastTickets/FastTickets/App_Start/RouteConfig.cs
@@ -14,6 +14,18 @@ namespace FastTickets
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "Evento",
+               url: "Evento/{action}/{id}",
+               defaults: new { controller = "Evento", action = "Index", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "Local",
+               url: "Local/{action}/{id}",
+               defaults: new { controller = "LocalEvento", action = "Index", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
